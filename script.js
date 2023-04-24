b735de0e7dbfc0900ab1b45624ae538d936f9d07
Michaela Brown <michaelarbrown4@gmail.com>
2023-04-23 19:51:19 -0500

    added base html and js files

---
+++ script.js
@@ -0,0 +1,4 @@
+var key = "71a47e531d4c080f7770f0bd1e588da9";
+var BASE_URL = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=71a47e531d4c080f7770f0bd1e588da9";
+var BASE_GEO_URL = "http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit=1&appid=71a47e531d4c080f7770f0bd1e588da9";
+
