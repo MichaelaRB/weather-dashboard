c4f926852afc634d543172ebbb8e309a85ed3d0d
Michaela Brown <michaelarbrown4@gmail.com>
2023-04-23 23:46:45 -0500

    added a function that will take the geographical data to get the weather forecast for the city

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var key = "71a47e531d4c080f7770f0bd1e588da9";
-var BASE_URL = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=71a47e531d4c080f7770f0bd1e588da9";
+var BASE_URL = "https://api.openweathermap.org/data/2.5/forecast?lat=";
 var locUrl = "http://api.openweathermap.org/geo/1.0/direct?q=";
 var coordinates = [];
 var latitude;
@@ -8,9 +8,8 @@ var longitude;
 var inputEl = $("#searchBtn");
 
 function getLocation(city) {
-    locUrl = "http://api.openweathermap.org/geo/1.0/direct?q=";
-    locUrl = locUrl + city + "&appid=71a47e531d4c080f7770f0bd1e588da9";
-    fetch(locUrl)
+    var geoUrl = locUrl + city + "&appid=71a47e531d4c080f7770f0bd1e588da9";
+    fetch(geoUrl)
         .then(function (response) {
             return response.json();
         })
@@ -21,13 +20,24 @@ function getLocation(city) {
         });
 }
 
+function getForecast(lat, lon) {
+    var weatherUrl = BASE_URL + lat + "&lon=" + lon + "&units=imperial&appid=71a47e531d4c080f7770f0bd1e588da9";
+    fetch(weatherUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+        });
+}
+
+
 inputEl.on('click', function(e) {
     e.preventDefault();
     var city = inputEl.parent().children("#cityInput").val();
     getLocation(city);
-    console.log(city);
     setTimeout(() => {
-        console.log(latitude);
+        getForecast(latitude, longitude);
     }, 1000);
 })
 
