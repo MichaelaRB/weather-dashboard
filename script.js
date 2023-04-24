beadcebe7c54126f4b5934779dc5359dc9e98a75
Michaela Brown <michaelarbrown4@gmail.com>
2023-04-23 22:44:07 -0500

    added code to pull location data, added style.css file, linked bootstrap

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
 var key = "71a47e531d4c080f7770f0bd1e588da9";
 var BASE_URL = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=71a47e531d4c080f7770f0bd1e588da9";
-var BASE_GEO_URL = "http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit=1&appid=71a47e531d4c080f7770f0bd1e588da9";
+var locUrl = "http://api.openweathermap.org/geo/1.0/direct?q=";
+var coordinates = [];
+var latitude;
+var longitude;
 
+function getLocation(city) {
+    locUrl = locUrl + city + "&appid=71a47e531d4c080f7770f0bd1e588da9";
+    fetch(locUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            latitude = data[0].lat;
+            longitude = data[0].lon;
+        });
+}
+getLocation("Shawnee");
+setTimeout(() => {
+    console.log(latitude);
+}, 1000);
